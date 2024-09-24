8e8cd4f33ec5d3fbc2c08fe765c1b59ba36f3beb
soimalfath <soimkmpbg@gmail.com>
2024-09-24 08:35:50 +0700

    add global prefix

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   console.log('NODE_ENV:', process.env.NODE_ENV);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api/v1');
   const corsOptions: CorsOptions = {
     origin: process.env.FRONT_END_URL, // Mengizinkan asal ini
     credentials: true, // Jika menggunakan cookies atau header otentikasi

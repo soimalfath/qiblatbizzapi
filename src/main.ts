80c563d65cfd7f78b8e027492f9e51d29b24f7fe
soimalfath <soimkmpbg@gmail.com>
2024-10-16 08:34:19 +0700

    role back cors in app

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,10 @@ async function bootstrap() {
   console.log('NODE_ENV:', process.env.NODE_ENV);
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
-    origin: process.env.FRONT_END_URL, // Mengizinkan asal ini
-    credentials: true, // Jika menggunakan cookies atau header otentikasi
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'Accept',
-      'Origin',
-      'User-Agent',
-    ],
+    origin: process.env.FRONT_END_URL,
+    credentials: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   };
   app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
